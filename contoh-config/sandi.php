59337d1aa1411a61501e129696efcef2136aa7d0
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2021-09-24 07:00:47 +0700

    Tambahkan contoh config yg digunakan

---
+++ contoh-config/sandi.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['nothing'] = '';
+$config['abaikan'] = '';
+$config['dev_token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6bnVsbCwidGltZXN0YW1wIjoxNjAzNDY2MjM5fQ.HVCNnMLokF2tgHwjQhSIYo6-2GNXB4-Kf28FSIeXnZw';
+$config['mapbox_token'] = '';
