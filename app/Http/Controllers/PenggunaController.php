bb86a23cdbe2f5edffac1857642a446b843d469e
firlianif <firlianif@gmail.com>
2022-05-16 17:46:35 +0700

    hapus use Illuminate\Support\Facades\Validator

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\DataTables;
 
 class PenggunaController extends Controller
