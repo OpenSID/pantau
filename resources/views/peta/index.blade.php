885046d7e07fdd2ea84885ddcd9343ba2caede78
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-07-13 17:26:17 +0800

    Update index.blade.php
    Update index.blade.php
    
    as

--- resources/views/peta/index.blade.php
+++ resources/views/peta/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.index')
 @include('layouts.components.select2_wilayah')
 
+@push('css')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+        integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+        crossorigin="" />
+    <style>
+        #map {
+            width: 100%;
+            height: 63vh;
+        }
+    </style>
+@endpush
 @section('title', 'Desa Pengguna OpenSID')
 
 @section('content_header')
@@ -10,62 +21,33 @@
 @section('content')
     <div class="card card-outline card-info">
         <div class="card-body">
-            <div class="row">
-                <div class="col-sm">
-                    <div class="form-group">
-                        <label>Provinsi</label>
-                        <select class="select2 form-control-sm" id="provinsi" name="provinsi"
-                            data-placeholder="Pilih Provinsi" style="width: 100%;">
-                        </select>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="form-group">
-                        <label>Kabupaten</label>
-                        <select class="select2 form-control-sm" id="kabupaten" name="kabupaten"
-                            data-placeholder="Pilih Kabupaten" style="width: 100%;">
-                        </select>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="form-group">
-                        <label>Kecamatan</label>
-                        <select class="select2 form-control-sm" id="kecamatan" name="kecamatan"
-                            data-placeholder="Pilih Kecamatan" style="width: 100%;">
-                        </select>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <div class="form-group">
-                        <label>Desa</label>
-                        <select class="select2 form-control-sm" id="desa" name="desa" data-placeholder="Pilih Desa"
-                            style="width: 100%;">
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div id="map" style="width: 1800px; height: 400px;">
-                </div>
-            </div>
+            <div id="map"></div>
         </div>
     </div>
 @endsection
 
 @section('js')
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+        integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+        crossorigin=""></script>
     <script>
-        $(function() {
-            var map = L.map('map').setView([51.505, -0.09], 13);
+        $(document).ready(function() {
+            var token = "{{ config('tracksid.sandi.mapbox_token') }}";
+            var mapCenter = [
+                {{ config('leaflet.map_center_latitude') }},
+                {{ config('leaflet.map_center_longitude') }}
+            ];
 
-            var tiles = L.tileLayer(
-                'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={{ config('tracksid.sandi.mapbox_token') }}', {
-                    maxZoom: 18,
-                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    id: 'mapbox/streets-v11',
-                    tileSize: 512,
-                    zoomOffset: -1
-                }).addTo(map);
+            // {{ request('latitude', config('leaflet.map_center_latitude')) }},
+            // {{ request('longitude', config('leaflet.map_center_longitude')) }}
+
+            var map = L.map('map').setView(mapCenter, {{ config('leaflet.zoom_level') }});
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap'
+            }).addTo(map);
         });
     </script>
 @endsection
