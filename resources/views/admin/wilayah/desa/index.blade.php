472a36fa13b59d827391ad0cf67a6de186b76287
Andi Fahruddin Akas <andifahruddinakas@gmail.com>
2022-07-18 12:22:56 +0800

    Update index.blade.php

--- resources/views/admin/wilayah/desa/index.blade.php
+++ resources/views/admin/wilayah/desa/index.blade.php
@@ -13,14 +13,10 @@
         <div class="col-lg-12">
             <div class="card card-outline card-primary">
                 <div class="card-header with-border">
-                    <<<<<<< HEAD <a href="{{ url('desa/create') }}" class="btn btn-success btn-sm mb-3"><i
-                            class="fas fa-plus"></i>
+                    <a href="{{ route('desa.create') }}" class="btn btn-success btn-sm mb-3"><i class="fas fa-plus"></i>
                         &ensp;Tambah</a>
-                        <a href="{{ url('desa/import') }}" class="btn btn-primary btn-sm mb-3"><i class="fas fa-upload"></i>
-                            &ensp;Import</a>
-                        =======
-                        <a href="{{ url('desa/create') }}" class="btn btn-primary btn-sm mb-3">Tambah Data</a>
-                        >>>>>>> dev
+                    <a href="{{ route('desa.import') }}" class="btn btn-primary btn-sm mb-3"><i class="fas fa-upload"></i>
+                        &ensp;Import</a>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
