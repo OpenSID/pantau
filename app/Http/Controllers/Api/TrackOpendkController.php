754be5cd8847c5c0debc3b5c15b8c1c4b321c51d
apidong <apidong@users.noreply.github.com>
2022-09-22 03:15:22 +0000

    [ci skip] Fix styling

--- app/Http/Controllers/Api/TrackOpendkController.php
+++ app/Http/Controllers/Api/TrackOpendkController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
-use Exception;
-use App\Models\Opendk;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TrackOpendkRequest;
+use App\Models\Opendk;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class TrackOpendkController extends Controller
 {
-    public function __invoke(TrackOpendkRequest $request){
+    public function __invoke(TrackOpendkRequest $request)
+    {
         DB::beginTransaction();
 
         try {
@@ -21,7 +21,8 @@ class TrackOpendkController extends Controller
                 $request->requestData()
             );
             DB::commit();
-            return response()->json(['status'=> '-']);
+
+            return response()->json(['status' => '-']);
         } catch (Exception $e) {
             Log::error($e);
             DB::rollBack();
