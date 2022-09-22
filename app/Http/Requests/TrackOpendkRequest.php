4489d123bb8570b92a3b5cff16e5256ca5f3328e
apidong <apidong@users.noreply.github.com>
2022-09-22 18:44:25 +0000

    [ci skip] Fix styling

--- app/Http/Requests/TrackOpendkRequest.php
+++ app/Http/Requests/TrackOpendkRequest.php
@@ -78,7 +78,7 @@ class TrackOpendkRequest extends FormRequest
             'jumlah_penduduk',
             'jumlah_keluarga',
             'peta_wilayah',
-            'sebutan_wilayah'
+            'sebutan_wilayah',
         ]);
     }
 }
