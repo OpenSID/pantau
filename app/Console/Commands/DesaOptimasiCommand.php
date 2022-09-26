e97b0d869696dfd0626822184a87ff217f12b145
apidong <apidong@users.noreply.github.com>
2022-09-26 15:15:07 +0000

    [ci skip] Fix styling

--- app/Console/Commands/DesaOptimasiCommand.php
+++ app/Console/Commands/DesaOptimasiCommand.php
@@ -50,7 +50,7 @@ class DesaOptimasiCommand extends Command
                 'versi_lokal',
                 'tgl_akses_lokal',
             ])
-            ->where('url_hosting', '<>' ,"")
+            ->where('url_hosting', '<>', '')
             ->whereNotNull('url_hosting')
             ->whereRaw("(CASE WHEN ((url_lokal Like 'localhost%' || url_lokal Like '10.%' || url_lokal Like '127.%' || url_lokal Like '192.168.%' || url_lokal Like '169.254.%' || url_lokal REGEXP '(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)')) THEN 1 ELSE 0 END) = 0")
             ->dd();
