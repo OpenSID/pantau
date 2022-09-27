ebc8daa5aa173567ec990f950bb68d3d694eb08e
Agung Sugiarto <sugiartoagung92@gmail.com>
2022-09-27 10:53:22 +0700

    Hapus transaction

--- app/Http/Controllers/Api/TrackOpendkController.php
+++ app/Http/Controllers/Api/TrackOpendkController.php
@@ -6,26 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TrackOpendkRequest;
 use App\Models\Opendk;
 use Exception;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class TrackOpendkController extends Controller
 {
     public function __invoke(TrackOpendkRequest $request)
     {
-        DB::beginTransaction();
-
         try {
             Opendk::query()->updateOrCreate(
                 $request->requestWhere(),
                 $request->requestData()
             );
-            DB::commit();
 
             return response()->json(['status' => true]);
         } catch (Exception $e) {
             Log::error($e);
-            DB::rollBack();
 
             return response()->json('Failed', 422);
         }
