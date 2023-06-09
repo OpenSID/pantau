329d2e118e02aa7acf78422be83d87684213e75e
Agung Sugiarto <sugiartoagung92@gmail.com>
2023-06-09 15:30:25 +0700

    Validasi (#164)
    [bug] perbaiki validasi dan modify column

---
+++ database/migrations/2023_06_01_111807_alter_table_desa_kodepos_lat_lng.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableDesaKodeposLatLng extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('desa', function (Blueprint $table) {
+            $table->string('kode_pos', 6)->nullable()->change();
+            $table->string('lat', 20)->nullable()->change();
+            $table->string('lng', 20)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('desa', function (Blueprint $table) {
+            $table->string('kode_pos', 6)->change();
+            $table->string('lat', 20)->change();
+            $table->string('lng', 20)->change();
+        });
+    }
+}
