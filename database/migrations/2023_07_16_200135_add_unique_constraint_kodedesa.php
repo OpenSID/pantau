19e3824d671a21131113e61734df3086754e90f8
ahmad afandi <ahmad.afandi85@gmail.com>
2023-07-16 20:39:51 +0700

    [fix] bersihkan data lalu tambahkan constraint unique pada kolom kode_desa table desa

---
+++ database/migrations/2023_07_16_200135_add_unique_constraint_kodedesa.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Desa;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class AddUniqueConstraintKodedesa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $kembar = DB::table('desa')
+                ->groupBy('kode_desa')
+                ->having(DB::raw('count(kode_desa)'), '>', 1)
+                ->pluck('kode_desa');
+        if ($kembar) {
+            $awal = [];
+            $hapus = Desa::select(['id', 'kode_desa', 'updated_at'])->whereIn('kode_desa', $kembar->toArray())->orderBy('updated_at', 'desc')->get()->groupBy('kode_desa')->map(function($item) {
+                $item->shift();
+                return $item->pluck('id');
+            });
+            Desa::whereIn('id', $hapus->flatten()->all())->delete();
+        }
+
+        Schema::table('desa', function(Blueprint $table){
+            $table->unique('kode_desa', 'uq_desa_kode_desa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('desa', function(Blueprint $table){
+        //     $table->dropUnique('uq_desa_kode_desa');
+        // });
+    }
+}
