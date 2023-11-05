316aa664c54297c04d1801b1bc68bff8459d841f
sarwono <scqolbu@gmail.com>
2023-11-05 12:08:22 +0700

    backup db dan storage

--- app/Console/Commands/BackupDatabaseStorage.php
+++ app/Console/Commands/BackupDatabaseStorage.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
+use Spatie\DbDumper\Databases\MySql;
 
 class BackupDatabaseStorage extends Command
 {
@@ -19,6 +21,9 @@ class BackupDatabaseStorage extends Command
      * @var string
      */
     protected $description = 'Melakukan Backup Database dan folder storage (dapat dilakukan melaluo cronjob)';
+
+    private $folder_database;
+    private $database_name = 'db_pantau.sql';
 
     /**
      * Create a new command instance.
@@ -37,6 +42,33 @@ class BackupDatabaseStorage extends Command
      */
     public function handle()
     {
-        return 0;
+        $this->folder_database = folderBackupDatabase();
+        $this->backupDatabase();
+        $this->backupStorage();
+    }
+
+    private function backupDatabase()
+    {
+        try {
+            $backup = MySql::create()
+                ->setDbName(env('DB_DATABASE'))
+                ->setUserName(env('DB_USERNAME'))
+                ->setPassword(env('DB_PASSWORD'));
+
+            $backup->dumpToFile($this->folder_database . '/' . $this->database_name);
+        } catch (Exception $ex) {
+            var_dump("Peringatan : gagal backup ke database Pantau, silakan cek koneksi !!!");
+            return exec('rm ' . $this->folder_database . '/' . $this->database_name);
+        }
+    }
+
+    private function backupStorage()
+    {
+        $folderdesa_from = 'storage';
+        $folderdesa_to = folder_backup() . DIRECTORY_SEPARATOR . 'storage';
+
+        if (file_exists($folderdesa_from)) {
+            exec('cp -R ' . $folderdesa_from . ' ' . $folderdesa_to);
+        }
     }
 }
