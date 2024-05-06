90b6c193a7a3b5034b0888ecca41fa69def9ea0b
Agung Sugiarto <sugiartoagung92@gmail.com>
2024-05-06 11:20:36 +0700

    Perbaiki validasi penambahan region desa

--- app/Http/Requests/RegionRequest.php
+++ app/Http/Requests/RegionRequest.php
@@ -24,7 +24,13 @@ class RegionRequest extends FormRequest
     public function rules()
     {
         return [
-            'region_code' => 'required|string|min:2|max:15',
+            'region_code' => [
+                'required',
+                'string',
+                'min:2',
+                'max:15',
+                "unique:tbl_regions,region_code,{$this->route('desa')}"
+            ],
             'region_name' => 'required|string|min:1|max:80',
             'parent_code' => 'required|string|min:2|max:15',
             'keterangan' => 'max:250',
