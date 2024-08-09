0f01f8f49dfdc0da16a656c5dc312555dbc02976
Yogi Perdana <yogiperdana3@gmail.com>
2024-08-09 09:58:53 +0700

    Sesuaikan visual statistik

--- resources/views/website/partial/chart.blade.php
+++ resources/views/website/partial/chart.blade.php
@@ -1,10 +1,23 @@
+@push('css')
+    <style>
+        .chart-container {
+            width: 100%;
+            height: 90vh; /* Atur tinggi sesuai kebutuhan */
+        }
+
+        #myChart {
+            width: 100%; /* Mengisi kontainer */
+            height: 100%; /* Mengisi kontainer */
+        }
+    </style>
+@endpush
 <div class="row mt-4">
     <div class="col-md-12">
     <div class="card">
         <div class="card-header">
             <h3>Pengguna Aktif Aplikasi OpenDesa</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body chart-container">
             <canvas id="myChart"></canvas>
         </div>
     </div>
@@ -16,7 +29,31 @@
     const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
         type: 'line',
-        data: {}
+        data: {},
+        options: {
+            layout: {
+                padding: 20
+            },
+            elements: {
+                point: {
+                    radius: 5
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        padding: 10
+                    }
+                },
+                y: {
+                    ticks: {
+                        padding: 10
+                    }
+                }
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false
     });    
 </script>
 @endpush
