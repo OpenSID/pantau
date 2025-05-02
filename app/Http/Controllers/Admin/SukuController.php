a0c585e6b7a5559869c49f4c9d6cdba1563d4c02
ahmad afandi <ahmad.afandi85@gmail.com>
2025-05-02 07:51:55 +0700

    Rilis v2505.0.0 (#480)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Abah Roland <59082428+vickyrolanda@users.noreply.github.com>
    Co-authored-by: Firliani Fauziah <firlianif@gmail.com>
    Co-authored-by: Agung Sugiarto <sugiartoagung92@gmail.com>
    Co-authored-by: Afila <apidongblog@gmail.com>
    Co-authored-by: sarwono <scqolbu@gmail.com>
    Co-authored-by: scqolbu24 <scqolbu24@users.noreply.github.com>
    Co-authored-by: agungsugiarto <agungsugiarto@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: misarianto <amikrian8@gmail.com>
    Co-authored-by: misarianto <misarianto@users.noreply.github.com>

---
+++ app/Http/Controllers/Admin/SukuController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Exports\SukuExport;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SukuRequest;
+use App\Imports\SukuImport;
+use App\Models\Region;
+use App\Models\Suku;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+use Yajra\DataTables\DataTables;
+
+class SukuController extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->excel) {
+            $paramDatatable = json_decode($request->get('params'), 1);
+            $request->merge($paramDatatable);
+        }
+
+        if ($request->ajax() || $request->excel) {
+            $query = DataTables::of(Suku::with('region'));
+            if ($request->excel) {
+                $query->filtering();
+
+                return Excel::download(new SukuExport($query->results()), 'Wilayah-Suku.xlsx');
+            }
+
+            return $query->addIndexColumn()
+                ->addColumn('action', function ($data) {
+                    $edit = '<a href="'.route('suku.edit', $data->id).'" class="btn btn-sm btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></a>';
+                    $delete = '<button data-href="'.route('suku.destroy', $data->id).'" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#confirm-delete"><i class="fas fa-trash"></i></button>';
+
+                    return '<div class="btn btn-group">'.$edit.$delete.'</div>';
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('admin.suku.index');
+    }
+
+    public function create()
+    {
+        return view('admin.suku.create');
+    }
+
+    public function store(SukuRequest $request)
+    {
+        $input = $request->all();
+        $input['tbl_region_id'] = Region::where('region_code', $input['tbl_region_id'])->where('parent_code', 0)->first()->id;
+        if (Suku::create($input)) {
+            return redirect('suku')->with('success', 'Data berhasil disimpan');
+        }
+
+        return back()->with('error', 'Data gagal disimpan');
+    }
+
+    public function edit($id)
+    {
+        return view('admin.suku.edit', [
+            'suku' => Suku::with('region')->findOrFail($id),
+        ]);
+    }
+
+    public function update(SukuRequest $request, $id)
+    {
+        $input = $request->all();
+        $suku = Suku::findOrFail($id);
+
+        if ($suku->update($input)) {
+            return redirect('suku')->with('success', 'Data berhasil diubah');
+        }
+
+        return back()->with('error', 'Data gagal diubah');
+    }
+
+    public function import()
+    {
+        return view('admin.suku.import');
+    }
+
+    public function prosesImport(Request $request)
+    {
+        try {
+            Excel::import(new SukuImport, $request->file('file')->store('temp'));
+        } catch (\Exception $e) {
+            report($e);
+
+            return back()->with('error', 'Data gagal diimport');
+        }
+
+        // Hapus folder temp ketika sudah selesai
+        Storage::deleteDirectory('temp');
+
+        return redirect('suku')->with('success', 'Data berhasil diimport');
+    }
+
+    public function contohImport()
+    {
+        $file = public_path('/assets/import/data_suku.xlsx');
+
+        return response()->download($file);
+    }
+
+    public function destroy($id)
+    {
+        if (Suku::destroy($id)) {
+            return redirect('suku')->with('success', 'Data berhasil dihapus');
+        }
+
+        return redirect('suku')->with('error', 'Data gagal dihapus');
+    }
+}
---
+++ app/Imports/SukuImport.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Suku;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+
+class SukuImport implements ToModel, WithBatchInserts, WithUpserts, WithHeadingRow
+{
+    private $regionMap = [];
+    public function __construct()
+    {
+        ini_set('memory_limit', '-1');
+        $this->regionMap = \App\Models\Region::selectRaw('id, UPPER(region_name) as region_name')->where('parent_code',0)->pluck('id', 'region_name')->toArray();
+    }
+
+    public function batchSize(): int
+    {
+        return 500;
+    }
+
+    /**
+     * @return string|array
+     */
+    public function uniqueBy()
+    {
+        return ['tbl_region_id', 'name'];
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
+    public function model(array $row)
+    {
+        // Check if the region name exists in the map
+        $namaProvinsi = trim(strtoupper(str_replace('Provinsi','',$row['nama_provinsi'])));
+        $regionId = $this->regionMap[$namaProvinsi] ?? null;
+        if(!$regionId) {
+            return null; // Skip if region ID is not found
+        }
+        return new Suku([
+            'tbl_region_id' => $regionId,
+            'name' => $row['nama_suku'],
+        ]);
+    }
+}
