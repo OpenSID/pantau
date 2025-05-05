33090dadab40b890d749dd714880e130a5280a7e
ahmad afandi <ahmad.afandi85@gmail.com>
2025-05-05 10:59:53 +0700

    Penerapan CI/CD Setup Pipeline Otomatis untuk Aplikasi Pantau

--- resources/views/website/opendk/provinsi_pengguna.blade.php
+++ resources/views/website/opendk/provinsi_pengguna.blade.php
@@ -1,17 +1,19 @@
 @push('css')
-<style>
-    .table-custom {
-        width: 100%;
-    }
-    .table-custom td {
-        width: 10%;
-        border: none;
-        padding: 0;
-    }
-    .table-custom .progress {
-        width: 100%;
-    }
-</style>
+    <style>
+        .table-custom {
+            width: 100%;
+        }
+
+        .table-custom td {
+            width: 10%;
+            border: none;
+            padding: 0;
+        }
+
+        .table-custom .progress {
+            width: 100%;
+        }
+    </style>
 @endpush
 
 <div class="container mt-3">
@@ -20,49 +22,57 @@
             <table class="table table-responsive table-custom">
                 <tbody>
                     @php
-                    $maxTotal = $provinsi_pengguna->max('total');
-                    $chunks = $provinsi_pengguna->chunk(16);
+                        $maxTotal = $provinsi_pengguna->max('total');
+                        $chunks = $provinsi_pengguna->chunk(16);
                     @endphp
-                    @foreach($chunks[0] as $provinsi)
-                    <tr>
-                        <td>
-                            {{ $provinsi->nama_provinsi }}
-                        </td>
-                        <td>                            
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: {{ $provinsi->total/$maxTotal * 100 }}%" aria-valuenow="{{ $provinsi->total/$maxTotal * 100 }}" aria-valuemin="0" aria-valuemax="{{ $provinsi->total }}"></div>
-                                <div class="pt-2"> &nbsp;{{ $provinsi->total }}</div>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach                    
+                    @foreach ($chunks[0] ?? [] as $provinsi)
+                        <tr>
+                            <td>
+                                {{ $provinsi->nama_provinsi }}
+                            </td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar"
+                                        style="width: {{ ($provinsi->total / $maxTotal) * 100 }}%"
+                                        aria-valuenow="{{ ($provinsi->total / $maxTotal) * 100 }}" aria-valuemin="0"
+                                        aria-valuemax="{{ $provinsi->total }}"></div>
+                                    <div class="pt-2"> &nbsp;{{ $provinsi->total }}</div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
                 <tbody class="collapse" id="pengguna-lengkap">
-                @foreach($chunks as $chunk)
-                    @if($loop->first) @continue @endif
-                    @foreach($chunk as $provinsi)                    
-                    <tr>
-                        <td>
-                            {{ $provinsi->nama_provinsi }}
-                        </td>
-                        <td>                            
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: {{ $provinsi->total/$maxTotal * 100 }}%" aria-valuenow="{{ $provinsi->total/$maxTotal * 100 }}" aria-valuemin="0" aria-valuemax="{{ $provinsi->total }}"></div>
-                                <div class="pt-2"> &nbsp;{{ $provinsi->total }}</div>
+                    @foreach ($chunks as $chunk)
+                        @if ($loop->first)
+                            @continue
+                        @endif
+                        @foreach ($chunk as $provinsi)
+                            <tr>
+                                <td>
+                                    {{ $provinsi->nama_provinsi }}
+                                </td>
+                                <td>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar"
+                                            style="width: {{ ($provinsi->total / $maxTotal) * 100 }}%"
+                                            aria-valuenow="{{ ($provinsi->total / $maxTotal) * 100 }}" aria-valuemin="0"
+                                            aria-valuemax="{{ $provinsi->total }}"></div>
+                                        <div class="pt-2"> &nbsp;{{ $provinsi->total }}</div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <tr class="mt-4">
+                        <td class="text-center w-100" colspan="2">
+                            <div class="btn border mt-2">
+                                <a href="#pengguna-lengkap" data-toggle="collapse" role="button">Selengkapnya</a>
                             </div>
                         </td>
                     </tr>
-                    @endforeach
-                @endforeach                
-                </tbody>
-                <tfoot>
-                <tr class="mt-4">
-                    <td class="text-center w-100" colspan="2">
-                        <div class="btn border mt-2">
-                            <a href="#pengguna-lengkap" data-toggle="collapse" role="button">Selengkapnya</a>
-                        </div>
-                    </td>
-                </tr>
                 </tfoot>
             </table>
         </div>
@@ -70,17 +80,17 @@
 </div>
 
 @push('js')
-<script>
-    $(document).ready(function () {
-        $('a[href="#pengguna-lengkap"]').click(function(){
-            const _text = $(this).text()
-            let _defaultText = 'Selengkapnya'
-            if(_text == _defaultText){
-                _defaultText = 'Perkecil'
-            } 
+    <script>
+        $(document).ready(function() {
+            $('a[href="#pengguna-lengkap"]').click(function() {
+                const _text = $(this).text()
+                let _defaultText = 'Selengkapnya'
+                if (_text == _defaultText) {
+                    _defaultText = 'Perkecil'
+                }
 
-            $(this).text(_defaultText)
+                $(this).text(_defaultText)
+            })
         })
-    })
-</script>
+    </script>
 @endpush
