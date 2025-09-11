90d66dba2c12cedcd7ef16b824a8556560c8f934
ahmad afandi <ahmad.afandi85@gmail.com>
2025-09-11 15:29:00 +0700

    ganti dengan looping

--- resources/views/laporan/tema.blade.php
+++ resources/views/laporan/tema.blade.php
@@ -17,60 +17,21 @@
             <div class="card card-outline card-primary">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-3">
-                            <!-- small card -->
-                            <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>{{$natra}}</h3>
-                                    <p>Tema Natra</p>
+                        @foreach ($listTemaCard as $temaCard)
+                            <div class="col-lg-{{ ceil(12/count($listTemaCard)) }}">
+                                <!-- small card -->
+                                <div class="small-box {{ $temaCard['color'] }}">
+                                    <div class="inner">
+                                        <h3>{{ $temaCard['count'] }}</h3>
+                                        <p>Tema {{ $temaCard['name'] }}</p>
+                                    </div>
+                                    <div class="icon">
+                                        <i class="fas fa-user-plus"></i>
+                                    </div>
+                                    <a href="{{ url('laporan/tema') }}?tema={{ $temaCard['name'] }}&total={{ $temaCard['count'] }}" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
                                 </div>
-                                <div class="icon">
-                                    <i class="fas fa-shopping-cart"></i>
-                                </div>
-                                <a href="{{ url('laporan/tema') }}?tema=Natra" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3">
-                            <!-- small card -->
-                            <div class="small-box bg-success">
-                                <div class="inner">
-                                    <h3>{{$esensi}}</h3>
-                                    <p>Tema Esensi</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="ion ion-stats-bars"></i>
-                                </div>
-                                <a href="{{ url('laporan/tema') }}?tema=Esensi" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3">
-                            <!-- small card -->
-                            <div class="small-box bg-warning">
-                                <div class="inner">
-                                    <h3>{{$palanta}}</h3>
-                                    <p>Tema Palanta</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-user-plus"></i>
-                                </div>
-                                <a href="{{ url('laporan/tema') }}?tema=Palanta" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3">
-                            <!-- small card -->
-                            <div class="small-box bg-danger">
-                                <div class="inner">
-                                    <h3>{{$lestari}}</h3>
-                                    <p>Tema Lestari</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-user-plus"></i>
-                                </div>
-                                <a href="{{ url('laporan/tema') }}?tema=Lestari" class="small-box-footer">Lihat detail <i class="fas fa-arrow-circle-right"></i></a>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -85,12 +46,8 @@
                     <div class="card-tools">
                         <span data-toggle="tooltip" title=""
                             class="badge badge-primary">
-                            @if(request()->query('tema') == 'Esensi')
-                            {{$esensi}}
-                            @elseif(request()->query('tema') == 'Natra')
-                            {{$natra}}
-                            @elseif(request()->query('tema') == 'Palanta')
-                            {{$palanta}}
+                            @if(request()->query('total'))
+                            {{request()->query('total')}}
                             @else
                             {{$tema}}
                             @endif
