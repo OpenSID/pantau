56e4f7a01d9e4b09ee18d2d170b31d3e03d27ac6
habibie11 <habibie.mahbub@gmail.com>
2026-07-05 21:42:40 +0700

    install package laravel 13

--- config/app.php
+++ config/app.php
@@ -190,10 +190,6 @@ return [
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         App\Providers\CustomTelegramServiceProvider::class,
